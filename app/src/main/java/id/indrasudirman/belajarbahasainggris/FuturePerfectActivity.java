31603715d92ed1f1399fd05416ba02b65895a915
Jabrikos <indrabsudirman@gmail.com>
2020-12-30 09:19:13 +0700

    Future Perfect done in Layout

---
+++ app/src/main/java/id/indrasudirman/belajarbahasainggris/FuturePerfectActivity.java
@@ -0,0 +1,14 @@
+package id.indrasudirman.belajarbahasainggris;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FuturePerfectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_future_perfect);
+    }
+}

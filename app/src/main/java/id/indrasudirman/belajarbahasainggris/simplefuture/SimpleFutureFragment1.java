07bed748efb1c843b2d281db63aa8b20d02ae7da
Jabrikos <indrabsudirman@gmail.com>
2021-01-10 13:03:23 +0700

    Doing Simple Future Tense, Hal 1

--- app/src/main/java/id/indrasudirman/belajarbahasainggris/simplefuture/SimpleFutureFragment1.java
+++ app/src/main/java/id/indrasudirman/belajarbahasainggris/simplefuture/SimpleFutureFragment1.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.SpannableString;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.uncopt.android.widget.text.justify.JustifiedTextView;
 
 import id.indrasudirman.belajarbahasainggris.R;
 
@@ -14,6 +17,9 @@ import id.indrasudirman.belajarbahasainggris.R;
 public class SimpleFutureFragment1 extends Fragment {
 
     private View view;
+    private JustifiedTextView simpleFutureExplain;
+    private JustifiedTextView simpleFutureExample1;
+    private JustifiedTextView simpleFutureExample2;
 
 
     public SimpleFutureFragment1() {
@@ -27,6 +33,23 @@ public class SimpleFutureFragment1 extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_simple_future1, container, false);
 
+        initView();
+        mSpannableString();
+
         return view;
+    }
+
+    private void initView() {
+
+        simpleFutureExplain = view.findViewById(R.id.simpleFutureExplain);
+        simpleFutureExample1 = view.findViewById(R.id.simpleFutureExample1);
+        simpleFutureExample2 = view.findViewById(R.id.simpleFutureExample2);
+    }
+
+    private void mSpannableString() {
+        SpannableString[] spannableString = new SpannableString[]{
+                new SpannableString((getActivity().getResources().getString(R.string.simple_future_explain))),
+                new SpannableString((getActivity().getResources().getString(R.string.second_bullet_simple_future))),
+                new SpannableString((getActivity().getResources().getString(R.string.third_bullet_simple_future)))};
     }
 }

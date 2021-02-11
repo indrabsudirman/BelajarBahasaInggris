05c974cba1d4d2e5726c8296542e252e0d3722b6
indrabsudirman <indrabsudirman@gmail.com>
2021-02-12 05:19:11 +0700

    Still doing on Present Perfect Tense 1

--- app/src/main/java/id/indrasudirman/belajarbahasainggris/presentperfect/PresentPerfectFragment1.java
+++ app/src/main/java/id/indrasudirman/belajarbahasainggris/presentperfect/PresentPerfectFragment1.java
@@ -1,12 +1,24 @@
 package id.indrasudirman.belajarbahasainggris.presentperfect;
 
+import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.uncopt.android.widget.text.justify.JustifiedTextView;
 
 import id.indrasudirman.belajarbahasainggris.R;
 
@@ -14,6 +26,9 @@ import id.indrasudirman.belajarbahasainggris.R;
 public class PresentPerfectFragment1 extends Fragment {
 
     private View view;
+    private JustifiedTextView explainTheTense;
+    private JustifiedTextView exampleTheTense1;
+    private JustifiedTextView exampleTheTense2;
 
 
     public PresentPerfectFragment1() {
@@ -27,7 +42,116 @@ public class PresentPerfectFragment1 extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_present_perfect1, container, false);
 
+        initView();
+        mSpannableString();
 
         return view;
+    }
+
+    private void initView() {
+        explainTheTense = view.findViewById(R.id.explainTheTense);
+        exampleTheTense1 = view.findViewById(R.id.exampleTheTense1);
+        exampleTheTense2 = view.findViewById(R.id.exampleTheTense2);
+    }
+
+    private void mSpannableString() {
+        SpannableString[] spannableString = new SpannableString[]{
+                new SpannableString((getActivity().getResources().getString(R.string.past_perfect_explain))),
+                new SpannableString((getActivity().getResources().getString(R.string.second_bullet_past_perfect_tense))),
+                new SpannableString((getActivity().getResources().getString(R.string.third_bullet_past_perfect_tense)))};
+
+        ClickableSpan clickableSpanExplainTheTense = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                AlertDialog.Builder alertDialogSimplePast = new AlertDialog.Builder(getActivity());
+                //Set Title
+                alertDialogSimplePast.setTitle("Info");
+                alertDialogSimplePast.setMessage("Past Perfect Tense adalah salah satu dari 16 Tense (waktu) yang ada dalam Grammar Bahasa Inggris.")
+                        .setCancelable(false)
+                        .setIcon(R.mipmap.ic_info_green)
+                        .setPositiveButton("Sudah paham", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                                Toast.makeText(getActivity(), "Saya sudah paham", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogSimplePast.create();
+                alertDialog.show();
+            }
+        };
+
+        ClickableSpan clickableSpanExampleTheTense1 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                AlertDialog.Builder alertDialogSimplePast = new AlertDialog.Builder(getActivity());
+                //Set Title
+                alertDialogSimplePast.setTitle("Info");
+                alertDialogSimplePast.setMessage("Kata 'had looked' adalah bentuk dari Past Perfect Tense, terdiri dari rumus had + verb 3. looked dari kata kerja look artinya melihat/terlihat.")
+                        .setCancelable(false)
+                        .setIcon(R.mipmap.ic_info_green)
+                        .setPositiveButton("Sudah paham", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                                Toast.makeText(getActivity(), "Saya sudah paham", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogSimplePast.create();
+                alertDialog.show();
+            }
+        };
+
+        ClickableSpan clickableSpanExampleTheTense2 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                AlertDialog.Builder alertDialogSimplePast = new AlertDialog.Builder(getActivity());
+                //Set Title
+                alertDialogSimplePast.setTitle("Info");
+                alertDialogSimplePast.setMessage("Kata 'had arrived' adalah bentuk dari Past Perfect Tense, terdiri dari rumus had + verb 3. arrived dari kata kerja arrive artinya datang/tiba.")
+                        .setCancelable(false)
+                        .setIcon(R.mipmap.ic_info_green)
+                        .setPositiveButton("Sudah paham", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                                Toast.makeText(getActivity(), "Saya sudah paham", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogSimplePast.create();
+                alertDialog.show();
+            }
+        };
+
+        // explainTheTense
+        spannableString[0].setSpan(clickableSpanExplainTheTense,0,18,0);
+        // make text bold
+        spannableString[0].setSpan(new StyleSpan(Typeface.BOLD), 0, 18, 0);
+        // sudah selesai di masa lampau (past). Sebelum ...
+        spannableString[0].setSpan(new StyleSpan(Typeface.BOLD), 72, 154, 0);
+        // Past Perfect
+        spannableString[0].setSpan(new StyleSpan(Typeface.BOLD), 186, 199, 0);
+        // she had looked
+        spannableString[0].setSpan(new StyleSpan(Typeface.BOLD), 318, 333, 0);
+        //Penjelasannya
+        spannableString[0].setSpan(new StyleSpan(Typeface.BOLD), 419, 434, 0);
+        spannableString[0].setSpan(new UnderlineSpan(), 419, 434, 0);
+
+        explainTheTense.setMovementMethod(LinkMovementMethod.getInstance());
+        explainTheTense.setText(spannableString[0]);
+
+        // exampleTheTense1
+        spannableString[1].setSpan(clickableSpanExampleTheTense1,23,33,0);
+        // make text bold
+        spannableString[1].setSpan(new StyleSpan(Typeface.BOLD), 23,33, 0);
+        exampleTheTense1.setMovementMethod(LinkMovementMethod.getInstance());
+        exampleTheTense1.setText(spannableString[1]);
+
+        // exampleTheTense2
+        spannableString[2].setSpan(clickableSpanExampleTheTense2,7,18,0);
+        // make text bold
+        spannableString[2].setSpan(new StyleSpan(Typeface.BOLD), 7,18, 0);
+        exampleTheTense2.setMovementMethod(LinkMovementMethod.getInstance());
+        exampleTheTense2.setText(spannableString[2]);
     }
 }

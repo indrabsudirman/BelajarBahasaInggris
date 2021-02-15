7e912eaef2527b913123c6659ac957c589f0e38d
indrabsudirman <indrabsudirman@gmail.com>
2021-02-15 15:52:00 +0700

    Finished on Future Perfect Tense 1

--- app/src/main/java/id/indrasudirman/belajarbahasainggris/futureperfect/FuturePerfectFragment1.java
+++ app/src/main/java/id/indrasudirman/belajarbahasainggris/futureperfect/FuturePerfectFragment1.java
@@ -1,12 +1,24 @@
 package id.indrasudirman.belajarbahasainggris.futureperfect;
 
+import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.uncopt.android.widget.text.justify.JustifiedTextView;
 
 import id.indrasudirman.belajarbahasainggris.R;
 
@@ -14,6 +26,9 @@ import id.indrasudirman.belajarbahasainggris.R;
 public class FuturePerfectFragment1 extends Fragment {
 
     private View view;
+    private JustifiedTextView explainTheTense;
+    private JustifiedTextView exampleTheTense1;
+    private JustifiedTextView exampleTheTense2;
 
 
     public FuturePerfectFragment1() {
@@ -27,6 +42,117 @@ public class FuturePerfectFragment1 extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_future_perfect1, container, false);
 
+        initView();
+        mSpannableString();
+
         return view;
+    }
+
+    private void initView() {
+        explainTheTense = view.findViewById(R.id.explainTheTense);
+        exampleTheTense1 = view.findViewById(R.id.exampleTheTense1);
+        exampleTheTense2 = view.findViewById(R.id.exampleTheTense2);
+    }
+
+    private void mSpannableString() {
+        SpannableString[] spannableString = new SpannableString[]{
+                new SpannableString((getActivity().getResources().getString(R.string.future_perfect_explain))),
+                new SpannableString((getActivity().getResources().getString(R.string.second_bullet_future_perfect_tense))),
+                new SpannableString((getActivity().getResources().getString(R.string.third_bullet_future_perfect_tense)))};
+
+        ClickableSpan clickableSpanExplainTheTense = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                AlertDialog.Builder alertDialogSimplePast = new AlertDialog.Builder(getActivity());
+                //Set Title
+                alertDialogSimplePast.setTitle("Info");
+                alertDialogSimplePast.setMessage("Future Perfect Tense adalah salah satu dari 16 Tense (waktu) yang ada dalam Grammar Bahasa Inggris.")
+                        .setCancelable(false)
+                        .setIcon(R.mipmap.ic_info_green)
+                        .setPositiveButton("Sudah paham", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                                Toast.makeText(getActivity(), "Saya sudah paham", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogSimplePast.create();
+                alertDialog.show();
+            }
+        };
+
+        ClickableSpan clickableSpanExampleTheTense1 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                AlertDialog.Builder alertDialogSimplePast = new AlertDialog.Builder(getActivity());
+                //Set Title
+                alertDialogSimplePast.setTitle("Info");
+                alertDialogSimplePast.setMessage("Kata 'will have arrived' adalah bentuk dari Future Perfect Tense, terdiri dari rumus will + have + verb 3. Arrived dari kata kerja arrive artinya datang.")
+                        .setCancelable(false)
+                        .setIcon(R.mipmap.ic_info_green)
+                        .setPositiveButton("Sudah paham", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                                Toast.makeText(getActivity(), "Saya sudah paham", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogSimplePast.create();
+                alertDialog.show();
+            }
+        };
+
+        ClickableSpan clickableSpanExampleTheTense2 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                AlertDialog.Builder alertDialogSimplePast = new AlertDialog.Builder(getActivity());
+                //Set Title
+                alertDialogSimplePast.setTitle("Info");
+                alertDialogSimplePast.setMessage("Kata 'will have finished' adalah bentuk dari Future Perfect Tense, terdiri dari rumus will + have + verb 3. Finished dari kata kerja finish artinya selesai.")
+                        .setCancelable(false)
+                        .setIcon(R.mipmap.ic_info_green)
+                        .setPositiveButton("Sudah paham", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                                Toast.makeText(getActivity(), "Saya sudah paham", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                AlertDialog alertDialog = alertDialogSimplePast.create();
+                alertDialog.show();
+            }
+        };
+
+        // explainTheTense
+        spannableString[0].setSpan(clickableSpanExplainTheTense,0,20,0);
+        // make text bold
+        spannableString[0].setSpan(new StyleSpan(Typeface.BOLD), 0, 20, 0);
+        // akan selesai, di waktu masa depan
+        spannableString[0].setSpan(new StyleSpan(Typeface.BOLD), 74, 107, 0);
+        // Future Perfect
+        spannableString[0].setSpan(new StyleSpan(Typeface.BOLD), 138, 153, 0);
+        // will have arrived
+        spannableString[0].setSpan(new StyleSpan(Typeface.BOLD), 250, 267, 0);
+
+        //Penjelasannya
+        spannableString[0].setSpan(new StyleSpan(Typeface.BOLD), 334, 349, 0);
+        spannableString[0].setSpan(new UnderlineSpan(), 334, 349, 0);
+
+        explainTheTense.setMovementMethod(LinkMovementMethod.getInstance());
+        explainTheTense.setText(spannableString[0]);
+
+        // exampleTheTense1
+        spannableString[1].setSpan(clickableSpanExampleTheTense1,8,25,0);
+        // make text bold
+        spannableString[1].setSpan(new StyleSpan(Typeface.BOLD), 8,25, 0);
+        exampleTheTense1.setMovementMethod(LinkMovementMethod.getInstance());
+        exampleTheTense1.setText(spannableString[1]);
+
+        // exampleTheTense2
+        spannableString[2].setSpan(clickableSpanExampleTheTense2,4,22,0);
+        // make text bold
+        spannableString[2].setSpan(new StyleSpan(Typeface.BOLD), 4,22, 0);
+        exampleTheTense2.setMovementMethod(LinkMovementMethod.getInstance());
+        exampleTheTense2.setText(spannableString[2]);
     }
 }

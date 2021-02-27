d386e2a02f7ed2c76635d48806f046eec4432383
indrabsudirman <indrabsudirman@gmail.com>
2021-02-27 16:33:58 +0700

    Still doing desain layout, User Account, add list of tenses

--- app/src/main/java/id/indrasudirman/belajarbahasainggris/AccountActivity.java
+++ app/src/main/java/id/indrasudirman/belajarbahasainggris/AccountActivity.java
@@ -1,23 +1,35 @@
 package id.indrasudirman.belajarbahasainggris;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private AppCompatImageView editAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
+
         //Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+        editAccount = findViewById(R.id.editAccount);
 
         //Set Recycler View Learn English as default
         bottomNavigationView.setSelectedItemId(R.id.user_account);
@@ -36,6 +48,49 @@ public class AccountActivity extends AppCompatActivity {
                         return true;
                 }
                 return false;
+            }
+        });
+//        setToolbar("Indra");
+
+//        initCollapsingToolbar();
+
+        editAccount.setOnClickListener(view ->
+                Toast.makeText(getApplicationContext(), "Anda click icon edit",Toast.LENGTH_SHORT).show());
+            }
+
+
+
+
+    public void setToolbar(@Nullable String title) {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        CollapsingToolbarLayout collapsingToolbar = findViewById(R.id.collapsingToolbarLayout);
+        collapsingToolbar.setTitle(title);
+    }
+
+    private void initCollapsingToolbar() {
+        final CollapsingToolbarLayout collapsingToolbar =
+                findViewById(R.id.collapsingToolbarLayout);
+        collapsingToolbar.setTitle(" ");
+        AppBarLayout appBarLayout = findViewById(R.id.appBarLayout);
+        appBarLayout.setExpanded(true);
+
+        // hiding & showing the title when toolbar expanded & collapsed
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbar.setTitle(Html.fromHtml("<font color='#FFFFFF'>"+" User details"+"</font>"));
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbar.setTitle(" ");
+                    isShow = false;
+                }
             }
         });
     }
